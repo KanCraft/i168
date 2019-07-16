5def7139d29c6a1eef20606d9b761ee730d0c28f
Hiromu OCHIAI <otiai10@gmail.com>
2019-07-16 11:22:58 +0900

    deviceNameってモデルじゃなかったので修正

--- components/Layout.ts
+++ components/Layout.ts
@@ -1,13 +1,36 @@
-import Constants from 'expo-constants';
-import { StyleSheet, Dimensions, ViewStyle, ActionSheetIOS } from 'react-native';
+import { StyleSheet, Dimensions, ViewStyle, Platform } from 'react-native';
 import { ItemProps } from './SideBar/Item';
 
 const KanColleGameAspectRatio = (1200 / 720);
 const HomebarHeight = 22;
-const DeviceName: string = Constants.deviceName;
 
 let { width: WIDTH, height: HEIGHT } = Dimensions.get('window');
 if (WIDTH < HEIGHT) [HEIGHT, WIDTH] = [WIDTH, HEIGHT];
+
+/**
+ * これここにあるべきではないけど、今んとこprivateだしいいかな、って。
+ * https://developer.apple.com/library/archive/documentation/DeviceInformation/Reference/iOSDeviceCompatibility/Displays/Displays.html
+ */
+function getDeviceModel(): string {
+  const dimension = Dimensions.get('window');
+  if (Platform.OS == 'ios') {
+    const has = (pix: number): boolean => dimension.height == pix || dimension.width == pix;
+    if (Platform.isPad) {
+      if (has(1366)) return 'iPad Pro 12.9-inch';
+      if (has(834)) return 'iPad Pro 10.5-inch';
+      if (has(1024)) return 'iPad Pro';
+      return 'iPad';
+    } else {
+      if (has(896)) return 'iPhone XR';
+      if (has(812)) return 'iPhone X';
+      if (has(736)) return 'iPhone 8 Plus';
+      if (has(667)) return 'iPhone 8';
+      if (has(568)) return 'iPhone SE';
+      return 'iPhone';
+    }
+  }
+  return 'i168_UNKONW';
+};
 
 interface Styles {
   top: ViewStyle;
@@ -26,17 +49,20 @@ export function arrangeActions({ reload, screenshot }: { reload: ItemProps, scre
   right?: { top?: ItemProps[] },
   bottom?: { left: ItemProps[], right: ItemProps[] },
 } {
+
+  const model = getDeviceModel();
+
   const actions = {
     top: { left: [], right: [] },
     left: { top: [] }, right: { top: [] },
     bottom: { left: [], right: [] },
   };
-  if (/iPad Pro/i.test(DeviceName)) {
+  if (/iPad Pro/i.test(model)) {
     actions.top = { left: [reload], right: [screenshot] };
-  } else if (/iPhone X/i.test(DeviceName)) {
+  } else if (/iPhone X/i.test(model)) {
     actions.left = { top: [reload] };
     actions.right = { top: [screenshot] };
-  } else if (/iPhone SE/i.test(DeviceName)) {
+  } else if (/iPhone SE/i.test(model)) {
     actions.right = { top: [{ ...screenshot, size: 24 }, { ...reload, size: 24 }] };
   } else if ((WIDTH / HEIGHT) > KanColleGameAspectRatio) {
     actions.left = { top: [reload] };
@@ -52,6 +78,8 @@ export function arrangeActions({ reload, screenshot }: { reload: ItemProps, scre
  */
 const styles: Styles = (() => {
 
+  const model = getDeviceModel();
+
   const top: ViewStyle = {
     flexDirection: 'row', alignItems: 'center',
     paddingLeft: 24, paddingRight: 24,
@@ -64,7 +92,7 @@ const styles: Styles = (() => {
     paddingLeft: 24, paddingRight: 24,
   };
 
-  if (/iPad Pro/i.test(DeviceName)) {
+  if (/iPad Pro/i.test(model)) {
     /**
      * 艦これに比べて縦長なので、上下を満たして、左右はゼロ。
      */
@@ -74,7 +102,7 @@ const styles: Styles = (() => {
     right.flex = 0;
     game.width = WIDTH;
     game.height = WIDTH / KanColleGameAspectRatio;
-  } else if (/iPhone X/i.test(DeviceName)) {
+  } else if (/iPhone X/i.test(model)) {
     /**
      * 艦これに対して横長なので、左右は満たすけど、
      * ホームバーがあるので、上はゼロだけど下はホームバーのぶんだけ広げる。
@@ -85,7 +113,7 @@ const styles: Styles = (() => {
     bottom.height = HomebarHeight;
     game.height = HEIGHT - HomebarHeight;
     game.width = (HEIGHT - HomebarHeight) * KanColleGameAspectRatio;
-  } else if (/iPhone SE/i.test(DeviceName)) {
+  } else if (/iPhone SE/i.test(model)) {
     /**
      * 艦これに対して横長だけど、アクションボタンが配置できないので、
      * 右のバーだけ集中させて、上下と左はゼロ。
